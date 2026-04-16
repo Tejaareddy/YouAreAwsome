88141ceeb38748c337f0958bef94830c087a6bdc
Teja <muskuraviteja999@gmail.com>
2026-04-16 09:59:16 +0530

    Initial Commit

---
+++ YouAreAwsome/YouAreAwsomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwsomeApp.swift
+//  YouAreAwsome
+//
+//  Created by Raviteja Reddy on 16/04/26.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwsomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
